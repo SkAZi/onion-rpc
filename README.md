4f19d30a489b3b0a8f9d7f95657499e3c7d5f81c
Igor Potapov <scsiboy@gmail.com>
2014-09-16 15:44:45 +0200

    Fork from onion-common

---
+++ README.md
@@ -0,0 +1,4 @@
+OnionRpc
+========
+
+** TODO: Add description **
---
+++ mix.exs
@@ -0,0 +1,20 @@
+defmodule OnionRpc.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :onion_rpc,
+     version: "0.0.1",
+     elixir: "~> 1.0.0",
+     deps: deps]
+  end
+
+  def application do
+    [applications: [:logger, :onion_common]]
+  end
+
+  defp deps do
+    [
+      {:onion_common, github: "SkAZi/onion-common"}
+    ]
+  end
+end
